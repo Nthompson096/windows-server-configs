472843644ef27f0d497405cfb65c727908fc45a0
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-11 23:04:59 -0400

    Add files via upload

---
+++ sccm/Nics.ps1
@@ -0,0 +1,10 @@
+New-NetLbfoTeam -Name "Internet" -TeamMembers "Ethernet" -TeamingMode Static
+New-NetLbfoTeam -Name "Clients" -TeamMembers "Ethernet 2" -TeamingMode Static
+#
+#set the new IP address and DNS down here.
+#
+#
+#
+#
+New-NetIPAddress -IPAddress "192.168.1.1" -InterfaceAlias "Clients" -DefaultGateway "192.168.1.1" -PrefixLength 24
+Set-DnsClientServerAddress -InterfaceAlias "Clients" -ServerAddresses "127.0.0.1"
