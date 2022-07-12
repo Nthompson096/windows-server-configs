dc365bae4202e73bf02e1d169a77dd91284c1020
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-11 23:52:54 -0400

    Update ADDC_SCCM.ps1

--- sccm/ADDC_SCCM.ps1
+++ sccm/ADDC_SCCM.ps1
@@ -1,3 +1,40 @@
 Install-WindowsFeature -config .\ADDC-DHCP-DNS.XML
-.\SCCM_DEPLOY.ps1
-# .\SCCM-int.ps1
+
+Get-Module servermanager
+Install-WindowsFeature Web-Windows-Auth
+
+Install-WindowsFeature Web-ISAPI-Ext
+
+Install-WindowsFeature Web-Metabase
+
+Install-WindowsFeature Web-WMI
+
+Install-WindowsFeature BITS
+
+Install-WindowsFeature RDC
+
+Install-WindowsFeature Web-Asp-Net
+
+Install-WindowsFeature Web-Asp-Net45
+
+Install-WindowsFeature NET-HTTP-Activation
+
+Install-WindowsFeature NET-Non-HTTP-Activ
+
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "sccm.edu" `
+-DomainNetbiosName "SCCM0" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$true `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
