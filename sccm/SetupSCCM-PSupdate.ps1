957d79a6be22e7de8886f95c357797bfe60df3f7
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-11 22:39:54 -0400

    Add files via upload

---
+++ sccm/SetupSCCM-PSupdate.ps1
@@ -0,0 +1,25 @@
+#SetupSCCM script (include windows update module)
+#
+#
+#
+#Compiled by seperate scripts
+#Install script for PSwindowsUpdate; requires internet.
+#Use this if you want to install update via powershell; or just use sconfig.
+
+Install-Module -Name PSWindowsUpdate -Force
+
+Import-Module PSWindowsUpdate
+
+Set-DisplayResolution -Width 1920 -Height 1080 -Force
+
+Rename-Computer -NewName SCCM -LocalCredential Administrator -PassThru
+
+New-NetLbfoTeam -Name "Internet" -TeamMembers "Ethernet" -TeamingMode Static
+
+New-NetLbfoTeam -Name "Clients" -TeamMembers "Ethernet 2" -TeamingMode Static
+
+New-NetIPAddress -IPAddress "192.168.1.1" -InterfaceAlias "Clients" -DefaultGateway "192.168.1.1" -PrefixLength 24
+
+Set-DnsClientServerAddress -InterfaceAlias "Clients" -ServerAddresses "127.0.0.1"
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
