b2705d0276d706286021b94964bd29890d5cb0d4
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-14 17:55:37 -0400

    Update ADDC_SCCM.ps1

--- sccm/ADDC_SCCM.ps1
+++ sccm/ADDC_SCCM.ps1
@@ -29,16 +29,18 @@ Install-WindowsFeature NET-Non-HTTP-Activ
 #
 # Windows PowerShell script for AD DS Deployment, Stand-alone DC.
 #
-# Import-Module ADDSDeployment
-# Install-ADDSForest `
-# -CreateDnsDelegation:$false `
-# -DatabasePath "C:\Windows\NTDS" `
-# -DomainMode "WinThreshold" `
-# -DomainName "sccm.edu" `
-# -DomainNetbiosName "SCCM0" `
-# -ForestMode "WinThreshold" `
-# -InstallDns:$true `
-# -LogPath "C:\Windows\NTDS" `
-# -NoRebootOnCompletion:$true `
-# -SysvolPath "C:\Windows\SYSVOL" `
-# -Force:$true
+<# 
+Import-Module ADDSDeployment
+Install-ADDSForest `
+# createDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "sccm.edu" `
+-DomainNetbiosName "SCCM0" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$true `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+#>
