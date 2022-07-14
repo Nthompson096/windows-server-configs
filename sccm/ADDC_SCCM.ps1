d6795312d83cf174138b3f7ceb74f20832c191c6
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-14 17:38:08 -0400

    Update ADDC_SCCM.ps1

--- sccm/ADDC_SCCM.ps1
+++ sccm/ADDC_SCCM.ps1
@@ -2,8 +2,12 @@
 
 Get-Module servermanager
 
+# Use only if you want a stand-alone DC-DHCP-SCCM server
 # Install-WindowsFeature -config .\ADDC-DHCP-DNS.XML
-
+#
+# Add the computer to the domain of your choice in this script
+# add-Computer -DomainName Domain01 -Restart
+#
 Install-WindowsFeature Web-Windows-Auth
 
 Install-WindowsFeature Web-ISAPI-Ext
@@ -25,9 +29,8 @@ Install-WindowsFeature NET-HTTP-Activation
 Install-WindowsFeature NET-Non-HTTP-Activ
 
 #
-# Windows PowerShell script for AD DS Deployment
+# Windows PowerShell script for AD DS Deployment, Stand-alone DC.
 #
-
 # Import-Module ADDSDeployment
 # Install-ADDSForest `
 # -CreateDnsDelegation:$false `
