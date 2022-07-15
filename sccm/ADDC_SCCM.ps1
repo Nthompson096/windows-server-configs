0523d2ebb07a6d34c88ed495da423cc2613c3dcb
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-15 19:17:21 -0400

    Update ADDC_SCCM.ps1

--- sccm/ADDC_SCCM.ps1
+++ sccm/ADDC_SCCM.ps1
@@ -40,7 +40,7 @@ Install-ADDSForest `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$true `
+-NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
 #>
