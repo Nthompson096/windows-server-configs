fba101f4cb234d3d7636a0b2b67c4b180789fbe9
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-14 22:34:09 -0400

    Update SetupSCCM.ps1

--- sccm/SetupSCCM.ps1
+++ sccm/SetupSCCM.ps1
@@ -11,7 +11,9 @@ Import-Module PSWindowsUpdate
 #
 # Set-DisplayResolution -Width 1920 -Height 1080 -Force
 #
-<# Common display resolutions, pick your flavor and place them inside 'set-DisplayResolution', do not un-comment this block.
+<# 
+Common display resolutions, pick your flavor and place them inside 'set-DisplayResolution', do not un-comment this block.
+Useful if you are just running windows server in VNC instead of spice; or didn't have spice installed prior to oobe.
 640360
 800x600
 1536x864
