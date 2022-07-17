e49153311719e1d9e73ff07c8363691b8787fd47
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-16 21:13:48 -0400

    Update networking.ps1

--- Networking/networking.ps1
+++ Networking/networking.ps1
@@ -8,3 +8,9 @@
 #DNS Sever
 #
 # Install-WindowsFeature -config .\DNS.xml
+# 
+# Feel Free to add servers in this code block
+# Use Get-DhcpServerInDC for a list of DHCP servers
+<#
+Add-DhcpServerInDC -DnsName host.name.example -IPAddress x.x.x.x
+#>
