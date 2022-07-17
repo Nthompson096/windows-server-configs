67f497e8bf2e290da812c26d1bc8a8306e05f587
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-17 12:58:31 -0400

    Update ADDS.ps1

--- DC/ADDS.ps1
+++ DC/ADDS.ps1
@@ -4,3 +4,72 @@
 #Active Directory without a DNS installed or a delegation
 #
 #Install-WindowsFeature -config .\AD.xml
+
+# only use one of the following code blocks.
+<#
+# New Forest
+# Windows PowerShell script for AD DS Deployment, uncomment/edit some lines if you feel necessary.
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+# -CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+# Feel free to add a domain and domain bios name.
+# -DomainName " " `
+# -DomainNetbiosName " " `
+-ForestMode "WinThreshold" `
+# -InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+# -NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+#>
+
+<#
+# Add to doamin
+# Windows PowerShell script for AD DS Deployment, uncomment/edit some lines if you feel necessary.
+#
+
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+#-NoGlobalCatalog:$true `
+#-CreateDnsDelegation:$true `
+-Credential (Get-Credential) `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+# Feel free to add a domain and domain bios name.
+# -DomainName " " `
+# -InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+# -NoRebootOnCompletion:$false `
+# -ReplicationSourceDC " " `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+#>
+<#
+# New Existing domain
+# Windows PowerShell script for AD DS Deployment, uncomment/edit some lines if you feel necessary.
+#
+
+Import-Module ADDSDeployment
+Install-ADDSDomain `
+#-NoGlobalCatalog:$false `
+#-CreateDnsDelegation:$true `
+-Credential (Get-Credential) `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainType "ChildDomain" `
+#-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+# Feel free to add a domain, domain bios name and a parent domain
+# -NewDomainName " " `
+# -NewDomainNetbiosName " " `
+# -ParentDomainName " " `
+# -NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+#>
