0cb78f30e9404f04a3cc86486a1f3829ef7754e0
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-07-19 11:39:10 -0400

    Update SetupSCCM.ps1

--- sccm/SetupSCCM.ps1
+++ sccm/SetupSCCM.ps1
@@ -100,10 +100,19 @@ Invoke-WebRequest -Uri $source4 -OutFile $destination4
 New-NetLbfoTeam -Name "Internet" -TeamMembers "Ethernet" -TeamingMode Static
 New-NetLbfoTeam -Name "Clients" -TeamMembers "Ethernet 2" -TeamingMode Static
 #>
-#Sets a satic IP address and DNS address as loopback
 <# 
-New-NetIPAddress -IPAddress "192.168.1.1" -InterfaceAlias "Clients" -DefaultGateway "192.168.1.1" -PrefixLength 24
-Set-DnsClientServerAddress -InterfaceAlias "Clients" -ServerAddresses "127.0.0.1"
+New-NetLbfoTeam -Name "Internet" -TeamMembers "Ethernet" -TeamingMode SwitchIndependent
+New-NetLbfoTeam -Name "Clients" -TeamMembers "Ethernet 2" -TeamingMode SwitchIndependent
+#>
+<# 
+New-NetLbfoTeam -Name "Internet" -TeamMembers "Ethernet" -TeamingMode LACP
+New-NetLbfoTeam -Name "Clients" -TeamMembers "Ethernet 2" -TeamingMode LACP
+#>
+# Sets a satic IP address and DNS address as loopback
+# Set the values
+<# 
+New-NetIPAddress -IPAddress "x.x.x.x" -InterfaceAlias "Clients" -DefaultGateway "x.x.x.x" -PrefixLength xx
+Set-DnsClientServerAddress -InterfaceAlias "Clients" -ServerAddresses "x.x.x.x"
 #>
 # Will reboot the computer with no update.
 # shutdown /r
